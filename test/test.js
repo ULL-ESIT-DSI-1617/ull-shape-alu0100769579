2f0f5e77547e93faf6a8d555af60929163935f77
Raul Martin <alu0100769579@ull.edu.es>
2017-04-25 12:16:44 +0000

    Arreglando algunos fallos

--- test/test.js
+++ test/test.js
@@ -1,17 +1,19 @@
+"use strict";
 var Shape = require('../shapesarea.js');
-var Triangle = require('../triangle.js');
-var Square = require('../square.js');
-var Rectangle = require('../rectangle.js');
+var Triangle = require('@alu0100769579/ull-shape-triangle-alu0100769579.js');
+var Square = require('@alu0100769579/ull-shape-square-alu0100769579.js');
+var Rectangle = require('@alu0100769579/ull-shape-rectangle-alu0100769579.js');
+require("should");
 
 describe("getArea", function() {
   it("must compute the triangle area correctly. Shape object", function() {
     let a = new Shape({ width: 100, height: 50 }, 'Triangle');
     let s = a.getArea();
-    s.should.match(/^5000$/);
+    s.should.equal(2500);
   })
   it("must compute the triangle area correctly. Triangle object", function() {
     let a = new Triangle({ width: 100, height: 50 });
     let s = a.getArea();
-    s.should.match(/^5000$/);
+    s.should.equal(2500);
   })
 });
